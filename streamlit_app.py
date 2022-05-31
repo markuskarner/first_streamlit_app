84e1e10668acacc0b42ecdb6b5a840e6ba0e772a
markuskarner <markus.karner1994@gmail.com>
2022-05-31 10:08:45 +0200

    Update streamlit_app.py
    bugfix

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/da
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # User input
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.Index, ['Avocado','Strawberries']))
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado','Strawberries'])
 #streamlit.multiselect("Pick some fruits:", list(my_fruit_list.Fruit))
 
 streamlit.title('My Parents New Healthy Diner')
